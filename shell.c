b853d769fa0e293fd8bad884d372bb7a8a25fb2f
HEHADO <67066438+HEHADO@users.noreply.github.com>
2020-11-15 15:48:47 +0300

    Add files via upload

--- shell.c
+++ shell.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <limits.h>
 
 
 #define BLACK "\033[0m"
@@ -16,20 +18,21 @@
 
 
 char *readword (FILE *f,int* t){
-    int size,n = 0;
+    int n = 0;
+    int size =0;
     int quotesisopen = 0;
-    int ch;
-    char *word = malloc(2*sizeof(char));
-
+    char ch;
+    char* word = NULL;
+    word = malloc(0*sizeof(char));
     while (isspace(ch = getc(f)) );
     while ((!isspace(ch))||(quotesisopen != 0)||(ch == '"')){
         if (size <= n){
-            size = 2*size +1;
+            size = 2*size + 5;
             word = realloc(word,size);
         }
         if (ch == '"'){
             //printf("Kavichki\n");
-            if (quotesisopen == 0) quotesisopen = 1; //СПРОСИТЬ
+            if (quotesisopen == 0) quotesisopen = 1; // СПРОСИТЬ
             else if (quotesisopen == 1) quotesisopen = 0;
         }else{
             word[n] = ch;
@@ -38,7 +41,6 @@ char *readword (FILE *f,int* t){
         }
         ch = getc(f);
         if (ch == '\n'){
-
             *t = 1;
             goto m;
         }else
@@ -50,14 +52,15 @@ char *readword (FILE *f,int* t){
     m:
     if (quotesisopen != 0) fprintf(stderr,"%sI'll close it for u bro%s\n",RED,BLACK);
     if ((n==0)&&(ch!=EOF)&&(ispunct(ch))) word[n]=ch;
-    if (size <= n+1){
-        size = 2*size +1;
+    if (size <= n+10){
+        size = 2*size +10;
         word = realloc(word,size);
     }
     word[n+1]='\0';
+    word[n+2]='\0';
     return word;
 }
-
+/*
 int size (char* ch){
     int i=0;
     while (ch[i] !='\0')
@@ -66,52 +69,97 @@ int size (char* ch){
     }
     return i*sizeof(char);
 }
+*/
+
+char** createargv(int n,char* ch){
+    int size=1;
+    int i=0;
+    char** argv = malloc(1*sizeof(char*));
+    //printf("%s\n",ch);
+    //printf("%d\n",n);
+    while (n == 0){
+        if (size <= i+5){
+            size = 2*size + 5;
+            argv = realloc(argv,size);
+        }
+        //printf("%s\n",ch);
+        argv[i] = strdup(ch);
+        free(ch);
+        ch = readword(stdin, &n);
+        i++;
+        //printf("%d\n",i);
+    }
+    argv[i] = strdup(ch);
+    argv[i+1] = NULL;
+    return argv;
+}
+
+
+void runcommand(char** arg){
+    int status = 0;
+    if (fork()){
+        wait(&status);
+        if(status != 0) perror(arg[0]);
+    } else{
+        execvp(arg[0], arg);
+        //execv(getenv("PATH"),arg);
+        perror(arg[0]);
+        exit(2);
+    }
+}
+
+
+
+void myfree(char** v){
+    int i=0;
+    while (v[i] != NULL){
+        free (v[i]);
+        i++;
+    }
+    //free (v);
+}
+
 
 int main(){
-    char* ch,ch1=NULL;
-    char buff[1024];
+    char* ch1 = NULL;
+    char* ch = NULL;
+    char buff[PATH_MAX];
     int argc = 0;
     int n = 0;
     int size=1;
-    char** argv = malloc(1*sizeof(char*));
-    int fd[2];
-    //while 
+    char** argv = NULL;
+    int fd[2]; 
     for(;;){
-        printf("%s%s >%s",GREEN,getcwd(buff,1024),YELLOW);
+        l:
+        printf("%s%s > %s",GREEN,getcwd(buff,1024),YELLOW);
         ch = readword(stdin,&n);
         printf("%s",BLACK);
-        //rintf("%s\t%d\n",ch,n);
+                                            //printf("%s\t%d\n",ch,n);
         if (!strcmp(ch,C1)) {
             return 0;
             free(ch);
             exit;
         }
-
+        
         if (!strcmp(ch,C2)) {
             free(ch);
             ch = readword(stdin,&n);
             chdir(ch);
+            goto l;
+            free(ch);
         }
+        
 
-        //ch1 = malloc(sizeof(ch));
-        ch1 = ch;
-        while (!n){
-            if (size <= argc+1){
-                size = 2*size + 1;
-                argv = realloc(argv,size);
-            }
-            *argv[argc] = ch;
-            ch = readword(0,&n);
-            argc++;
+        int i = 0;
+        argv = createargv(n, ch);
+        //printf("%s",argv[0]);
+        while (argv[i] != NULL){
+            printf("%s\n",argv[i]);
+            i++;
         }
-        if (size <= argc+2){
-            size = 2*size + 1;
-            argv = realloc(argv,size);
-        }
-        *argv[argc++] = ch;
-        *argv[argc+1] = NULL;
-
-
+        runcommand(argv);
+        //myfree(argv);
+        /*
         if (pipe(fd)<0){
             perror("pipe");
             exit(1);
@@ -126,11 +174,16 @@ int main(){
                 close(fd[1]);
                 close(fd[0]);
                 execvp(ch1,argv);
+                //perror(ch1);
+                exit(2);
             default:
                 break;
             }
+            int status;
+            wait (&status);
+
             close(fd[0]);
             close(fd[1]);
-        }
+        }*/
     }    
 }
